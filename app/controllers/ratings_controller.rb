ba477ed37ff57e6c024731f86b148b6355df2a3e
Charlie Rivas <rivascharlie1@gmail.com>
2023-06-14 17:42:34 -0300

    ratings controller complete

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,61 @@
 class RatingsController < ApplicationController
 end
+  before_action :set_ratings, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :new, :create, :show, :edit]
+
+  def index
+    @rating = Rating.all
+  end
+
+  def show
+    @rating = Rating.find(@rating.movie_id)
+  end
+
+  def new
+    @rating = Rating.new
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def create
+    @rating = Rating.new(rating_params)
+    @movie = Movie.find(params[:movie_id])
+    @rating.movie_id = @movie.id
+    @rating.user_id = @user.id
+    @movie.status = true
+    if @rating.save && @movie.save
+      redirect_to rating_path(@rating.id, movie_id: @movie.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @rating.update(rating_params)
+      redirect_to rating_path(@rating.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @brating.destroy
+    redirect_to ratings_path, status: :see_other
+  end
+
+  private
+
+  def set_bookings
+    @rating = Rating.find(params[:id])
+  end
+
+  def rating_params
+    params.require(:rating).permit(:start_date, :end_date, :status, :yacht_id, :user_id)
+  end
+
+  def set_user
+    @user = current_user
+  end
+end
