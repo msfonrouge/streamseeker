a99bf96def4c47142de020a867f1f9fb227f2043
santiago lopez saubidet <slopez95@hotmail.com>
2023-06-15 19:01:53 -0300

    asociaciones de modelos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :ratings
+  has_many :reviews
+  has_many :movies, through: :ratings
+  has_many :movies, through: :reviews
+  has_many :watchlists, dependent: :destroy
+  has_many :markers, through: :watchlists, dependent: :destroy
 end
