577e4e323e56180de4842fd0bb78f4e5c26074a4
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-06-22 03:57:59 -0300

    Work on animated clicks

---
+++ app/javascript/controllers/favorites_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+
+export default class extends Controller {
+  static targets = ["heart"];
+
+  connect() {
+    //console.log(this.element);
+    //console.log(this.heartTarget);
+  }
+
+  toggleHeartButton() {
+    console.log("toggleHeartButton called");
+    this.heartTarget.classList.toggle("red");
+     this.heartTarget.style.color = "red";
+     document.body.style.backgroundColor = "#AA0000";
+  }
+}
